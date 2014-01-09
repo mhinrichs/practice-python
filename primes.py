6f6a47f6118d0a2d8fbeba017a383a1359a551ce
Matthew Hinrichs <mhinrichs@gmail.com>
2014-01-09 11:35:50 +0900

    Some practice python files

---
+++ primes.py
@@ -0,0 +1,8 @@
+# super simple prime generator based on the sieve of eratosthenes
+def primegen(max):
+	ar = [0 for x in range(max)]
+	ar[0], ar[1] = 1, 1 #eliminate base cases
+	for i in range(len(ar)):
+		if ar[i] == 0:
+			yield i
+			ar[i::i] = [1 for x in range(0, len(ar[i::i]))]
